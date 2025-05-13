aef535dae9d908dbbdafb4db02e2c3ce75cd1eac
Beau Hughes <hughesmoe3@gmail.com>
2025-05-12 20:18:29 -0700

    fixing better_auth_url env loading

--- apps/web/lib/auth/auth.ts
+++ apps/web/lib/auth/auth.ts
@@ -15,8 +15,16 @@ import * as dotenv from 'dotenv'
 dotenv.config({ path: path.resolve(process.cwd(), '../../.env') })
 const secret = process.env.BETTER_AUTH_SECRET
 
+let betterAuthUrl = process.env.BETTER_AUTH_URL
+
+if (!betterAuthUrl) {
+  const vercelUrl = process.env.NEXT_PUBLIC_VERCEL_URL
+  betterAuthUrl = vercelUrl ? `https://${vercelUrl}` : 'http://localhost:3000'
+}
+
 export const auth = betterAuth({
   secret,
+  betterAuthUrl,
   database: drizzleAdapter(db, {
     provider: 'pg',
     schema: {
